c48ae7cb172f4814407bc50f03c3895af3be5894
townsend <obadah2109@gmail.com>
2023-06-08 20:01:18 +0300

    staticfiels

--- website/settings.py
+++ website/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'topic' / 'static' / 'images'
 
